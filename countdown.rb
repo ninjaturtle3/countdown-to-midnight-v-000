15bdb7f33bd5418bab129c796164ed85021fc206
Lluvia Frias <lluvi.frias@gmail.com>
2019-03-27 03:16:23 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,14 @@
 #write your code here
+def countdown_with_sleep (num_secs)
+   sleep(5)
+end
 
-def countdown
+
+def countdown(n)
+n = 10
+    while n > 0
+      puts "#{n} SECOND(S)!"
+      n -= 1
+    end
+    return "HAPPY NEW YEAR!"
 end
