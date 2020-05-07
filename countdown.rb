37ebafbd9cc7e5f2d97ca2dfe8bcbe9485705046
Lluvia Frias <lluvi.frias@gmail.com>
2020-05-07 22:20:41 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,14 +1,48 @@
 #write your code here
-def countdown_with_sleep (num_secs)
-   sleep(5)
+def countdown(i)
+i = 10
+  while i > 0
+    puts "#{i} SECOND(S)!"
+    i -= 1
+  end
+return  "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(secs)
+  sleep(5)
 end
 
 
-def countdown(n)
-n = 10
-    while n > 0
-      puts "#{n} SECOND(S)!"
-      n -= 1
-    end
-    return "HAPPY NEW YEAR!"
-end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def countdown_with_sleep (num_secs)
+#    sleep(5)
+# end
+#
+#
+# def countdown(n)
+# n = 10
+#     while n > 0
+#       puts "#{n} SECOND(S)!"
+#       n -= 1
+#     end
+#     return "HAPPY NEW YEAR!"
+# end
